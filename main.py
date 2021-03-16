86a9ee1e176299ce1ebd42367ed67d9f2426f029
RianB99 <rianne.beelen@student.uva.nl>
2021-03-16 12:47:12 +0100

    add main.py file

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import time
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LinearRegression
+
+if __name__ == "__main__":
+    print("main funciton")
