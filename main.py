65c2390487e8ac4a97e2a6fc8e9710b444a6b407
RianB99 <rianne.beelen@student.uva.nl>
2021-03-16 14:08:33 +0100

    add function for reading training files

--- main.py
+++ main.py
@@ -6,5 +6,16 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LinearRegression
 
+def load_data():
+    df = pd.read_csv('./data/aug_train.csv', index_col='enrollee_id')
+    print(df.head(5))
+
+    # print niformation about nans
+    print("shape of dataframe:", df.shape, "\n")
+    print("number of nans per column:")
+    print(df.isna().sum(), "\n")
+    print("number of rows with amount of nans")
+    print(df.isna().sum(axis=1).value_counts())
+
 if __name__ == "__main__":
-    print("main funciton")
+    load_data()
