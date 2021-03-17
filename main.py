16305da58ebc77f895ebf5315748826b1ce068f9
JopRijks <31651172+JopRijks@users.noreply.github.com>
2021-03-17 13:53:00 +0100

    Template upgraded

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+from copy import deepcopy
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
@@ -9,13 +10,21 @@ from sklearn.linear_model import LinearRegression
 
 
 def load_data():
-    df_train = pd.read_csv('./data/aug_train.csv', index_col='enrollee_id')
+    definite_columns_no_na = [
+        'id','host_is_superhost', 'neighbourhood_cleansed', 'property_type', 'room_type', 'accommodates',
+        'bathrooms', 'bedrooms', 'beds', 'bed_type', 'amenities',
+        'price', 'guests_included', 'extra_people', 'minimum_nights',
+        'maximum_nights', 'number_of_reviews', 'cancellation_policy',
+    ]
+    df_train = pd.read_csv('./data/listings_details.csv', index_col='id',usecols=definite_columns_no_na)
+    df_train = df_train.dropna()
     return df_train, len(df_train.columns), len(df_train)
 
 def classify_data(df, variables, rows):
     start = time.time()
 
-    # classification code here
+    # regressor = LinearRegression()
+    # regressor.fit(X, y)
     return time.time() - start
 
 def plot_data(final_df):
@@ -25,8 +34,7 @@ def plot_data(final_df):
 
 if __name__ == "__main__":
     results = {}
-    #train_df, length_variables, length_rows = load_data()
-    train_df, length_variables, length_rows = 0, 10, 10000
+    train_df, length_variables, length_rows = load_data()
     for variables in range(int(length_variables/2),length_variables):
         rows_duration = {}
         for rows in range(int(length_rows/100), length_rows, int(length_rows/100)):
