b173476c582eae0dd175920f6ea7204c3fbbaa58
JopRijks <31651172+JopRijks@users.noreply.github.com>
2021-03-20 14:28:38 +0100

    Linear regression done,
    Plot needs to be done, i was thinking about plotly but the data results should be save saved in another format.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import plotly.express as px
 from copy import deepcopy
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
@@ -9,22 +11,39 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LinearRegression
 
 
-def load_data():
-    definite_columns_no_na = [
-        'id','host_is_superhost', 'neighbourhood_cleansed', 'property_type', 'room_type', 'accommodates',
-        'bathrooms', 'bedrooms', 'beds', 'bed_type', 'amenities',
-        'price', 'guests_included', 'extra_people', 'minimum_nights',
-        'maximum_nights', 'number_of_reviews', 'cancellation_policy',
-    ]
-    df_train = pd.read_csv('./data/listings_details.csv', index_col='id',usecols=definite_columns_no_na)
-    df_train = df_train.dropna()
-    return df_train, len(df_train.columns), len(df_train)
 
-def classify_data(df, variables, rows):
+def load_data():
+    df_train = pd.read_csv('./data/cleaned_listings.csv', index_col='id')
+    filter_col_prop = [col for col in df_train if col.startswith('property')]
+    filter_col_amenities = [col for col in df_train if col.startswith('amenities')]
+    filter_col_neighbourhood = [col for col in df_train if col.startswith('neighbourhood')]
+    one_hot = len(filter_col_prop) + len(filter_col_amenities) + len(filter_col_neighbourhood)
+    one_hot_cols = [filter_col_neighbourhood,filter_col_prop,filter_col_amenities]
+    return df_train, len(df_train.columns) - one_hot + 3, len(df_train), one_hot_cols
+
+def linear_regression(df, variables, rows, one_hot_cols):
+
+    df = df[:rows]
+    # define response variable
+    y = df["price"]
+
+    # define predictor variables
+    x = list(df.columns)
+    x.remove("price")
+    if variables >= 24:
+        x += one_hot_cols[0]
+        if variables >= 25:
+            x += one_hot_cols[1]
+            if variables >= 26:
+                x += one_hot_cols[2]
+    x = df[x[:variables]]
+
+    # start timer
     start = time.time()
 
-    # regressor = LinearRegression()
-    # regressor.fit(X, y)
+    # fit linear regression model
+    model = sm.OLS(y, x).fit()
+
     return time.time() - start
 
 def plot_data(final_df):
@@ -34,14 +53,17 @@ def plot_data(final_df):
 
 if __name__ == "__main__":
     results = {}
-    train_df, length_variables, length_rows = load_data()
-    for variables in range(int(length_variables/2),length_variables):
+    train_df, length_variables, length_rows, one_hot_cols = load_data()
+    for variables in range(int(length_variables/2),length_variables, 3):
         rows_duration = {}
         for rows in range(int(length_rows/100), length_rows, int(length_rows/100)):
-            duration = classify_data(train_df, variables, rows)
+            duration = linear_regression(train_df, variables, rows, one_hot_cols)
             rows_duration[rows] = duration
         results[variables] = rows_duration
 
     data_frame_results = pd.DataFrame(results)
+    data_frame_results.to_csv("results_linear_regression.csv")
     plot_data(data_frame_results)
+
+
 
